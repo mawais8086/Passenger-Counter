164310d378751b388b0c04688239ebfee9984af6
mawais8086 <122724269+mawais8086@users.noreply.github.com>
2023-01-26 02:57:44 +0500

    All files via upload

---
+++ Js/index.js
@@ -0,0 +1,30 @@
+document.getElementById("count-el").innerText=0
+
+let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
+
+
+
+console.log(countEl)
+
+
+
+let count=0
+function increment() {
+    count+=1
+    countEl.textContent=count
+}
+
+function save(){
+    console.log(count)
+}
+
+function save() {
+
+    let countstr= count+" - "
+saveEl.textContent+=countstr
+countEl.textContent=0
+count=0
+
+    
+}
